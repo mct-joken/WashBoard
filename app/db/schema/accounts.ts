6d5929668f639b1210d8c6f717f0bba56afd66ce
Tufusa <louptung@gmail.com>
2023-08-23 23:44:10 +0900

    chore: `email`カラムのインデックス作成記述を削除 unique制約が付いたtext型のカラムにはDrizzleが自動的にインデックスを付与しており、重複していたため

--- app/db/schema/accounts.ts
+++ app/db/schema/accounts.ts
@@ -1,21 +1,12 @@
 import { sql } from "drizzle-orm";
-import {
-  integer,
-  sqliteTable,
-  text,
-  uniqueIndex,
-} from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const accounts = sqliteTable(
-  "accounts",
-  {
-    id: text("id").primaryKey(),
-    email: text("email").notNull().unique(),
-    role: integer("role", { mode: "number" }).default(1), // 1: User, 2: Admin
-    createdAt: integer("createdAt", { mode: "timestamp_ms" })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: integer("updatedAt", { mode: "timestamp_ms" }),
-  },
-  (accounts) => ({ emailIdx: uniqueIndex("emailIdx").on(accounts.email) })
-);
+export const accounts = sqliteTable("accounts", {
+  id: text("id").primaryKey(),
+  email: text("email").notNull().unique(),
+  role: integer("role", { mode: "number" }).default(1), // 1: User, 2: Admin
+  createdAt: integer("createdAt", { mode: "timestamp_ms" })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: integer("updatedAt", { mode: "timestamp_ms" }),
+});
