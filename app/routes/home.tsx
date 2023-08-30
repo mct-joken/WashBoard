19e346e4b713d4ffd3302f76d1d6b25d46ac7b69
Suzune2741 <kannra1174@gmail.com>
2023-08-30 21:58:39 +0900

    add:selectboxでソートできるようにした

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,12 +3,13 @@ import styles from "../tailwind.css";
 import { json, LinksFunction } from "@remix-run/cloudflare";
 import { Link, useLoaderData } from "@remix-run/react";
 
-import { useState,useMemo } from "react";
-import Select from "react-select";
+import { useState, useMemo } from "react";
+import Menu from "./menu";
 
 //この辺でデータとか取ってきて自分の使用状況たしかめる
 const ngmsg = "なし";
 let okmsg = "使用中";
+
 const msg = () => {
   if (true) {
     return (
@@ -16,69 +17,120 @@ const msg = () => {
         <p className="text-center mb-5">{okmsg}</p>
         <Link to="/wash">
           <div className="flex justify-center items-center">
-            <p className="  w-20 text-center rounded-full bg-green-400 
-                        active:bg-green-700  hover:bg-green-700 py-1 px-5 text-white mr-3">
+            <p
+              className="  w-20 text-center rounded-full bg-green-400 
+                        active:bg-green-700  hover:bg-green-700 py-1 px-5 text-white mr-3"
+            >
               回収
             </p>
           </div>
         </Link>{" "}
-        {""}
       </div>
     );
   } else {
     return <p className="text-center mb-5">{ngmsg}</p>;
   }
 };
+
 const datas = [
   {
-    value: "042",
     label: "4棟2階",
     num: 2,
+    empty: 0,
   },
   {
-    value: "043",
     label: "4棟3階",
     num: 2,
+    empty: 0,
   },
   {
-    value: "051",
     label: "5棟1階",
     num: 1,
+    empty: 1,
   },
   {
-    value: "052",
     label: "5棟2階",
     num: 2,
+    empty: 0,
   },
   {
-    value: "053",
     label: "5棟3階",
     num: 1,
+    empty: 1,
   },
 ];
 
-export function showdata(free: number) {
-  for (let i = 0; i < datas.length; i++) {
-    if (free == 0) {
-      if (datas[i]["num"] == 1) return "満";
-      else return "空";
-    } else if (free == 1) return datas[i]["label"];
-  }
+export function showdata(selectdata: string) {
+  return datas.map((element) => {
+    if (selectdata == "empty" && element.empty == 0) {
+      return (
+        <div className="flex flex-row justify-center my-2.5 ">
+          <div className=" rounded-full bg-red-400 active:bg-red-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
+            満
+          </div>
+          <div className=" text-20 mr-2 text-lg">
+            <p>{element.label}</p>
+          </div>
+          <div className=" text-20 mr-2 text-lg">
+            <p>{element.num}</p>
+          </div>
+        </div>
+      );
+    } else if (selectdata == "all") {
+      if (element.empty == 1) {
+        return (
+          <div className="flex flex-row justify-center my-2.5 ">
+            <div className=" rounded-full bg-red-400 active:bg-red-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
+              満
+            </div>
+
+            <div className=" text-20 mr-2 text-lg">
+              <p>{element.label}</p>
+            </div>
+            <div className=" text-20 mr-2 text-lg">
+              <p>{element.num}</p>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="flex flex-row justify-center my-2.5 ">
+            <div className=" rounded-full bg-blue-400 active:bg-blue-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
+              空
+            </div>
+
+            <div className=" text-20 mr-2 text-lg">
+              <p>{element.label}</p>
+            </div>
+            <div className=" text-20 mr-2 text-lg">
+              <p>{element.num}</p>
+            </div>
+          </div>
+        );
+      }
+    } else return <></>;
+  });
+}
+export function setdata(value: string) {
+  let selectdata = value;
+  return selectdata;
 }
 
 export default function home() {
-  const [selectedValue, setSelectedValue] = useState(datas[0]);
+  let [data, setdata] = useState("all");
+  const dataChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setdata(e.target.value);
+  };
+
   return (
     <div className=" mt-2 mx-3">
       <div className="flex flex-row">
         <img src={Logo} alt="ロゴ" className=" h-10 mx-center my-auto"></img>
         <p className=" text-2xl  mx-auto my-auto">利用状況</p>
       </div>
-      <form>
-        <div className=" flex flex-row-reverse my-3">
-          <div></div>
-          <div></div>
-          <div className="w-20 mx-3">
+      <div className=" flex flex-row justify-center my-3">
+        <div className="w-20 mx-3">
+          <form>
             <input
               type="text"
               id="place_search"
@@ -90,158 +142,25 @@ export default function home() {
               required
             ></input>
             <div>
-              <Select
-                options={datas}
-                defaultValue={selectedValue}
-                onChange={(value) => {
-                  value ? setSelectedValue(value) : null;
-                }}
-
-              />
+              <select name="sort" id="sort" onChange={dataChange}>
+                <option value="all" selected>
+                  すべて
+                </option>
+                <option value="empty">空きあり</option>
+                <option value="favorite">お気に入り</option>
+              </select>
             </div>
-          </div>
+          </form>
         </div>
-      </form>
+      </div>
       <p className="border rounded mx-10"></p>
       <div className=" max-h-72 mx-5 px-5 my-5  overflow-y-auto">
-        {/*DBからデータ取れるようになるまで仮のデータ*/}
-
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-red-400 active:bg-red-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-            {showdata(0)}
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>{showdata(1)}</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>1台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-blue-400 active:bg-blue-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-            空
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>5棟3階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>1台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-red-400 active:bg-red-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-            満
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>4棟2階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>2台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-blue-400 active:bg-blue-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-            空
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>5棟3階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>1台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-red-400 active:bg-red-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-            満
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>4棟2階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>2台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-blue-400 active:bg-blue-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-            空
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>5棟3階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>1台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-red-400 active:bg-red-400   py-1 px-5 text-white mr-3 p-0 w-30 ">
-            満
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>4棟2階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>2台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-blue-400 active:bg-blue-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-            空
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>5棟3階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>1台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-red-400 active:bg-red-400   py-1 px-5 text-white mr-3 p-0 w-30 ">
-            満
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>4棟2階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>2台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-blue-400 active:bg-blue-400   py-1 px-5 text-white mr-3 p-0 w-30 ">
-            空
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>5棟3階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>1台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-red-400 active:bg-red-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-            満
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>4棟2階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>2台使用可</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center my-2.5 ">
-          <div className=" rounded-full bg-blue-400 active:bg-blue-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-            空
-          </div>
-          <div className=" text-20 mr-2 text-lg">
-            <p>5棟3階</p>
-          </div>
-          <div className=" text-20 text-lg">
-            <p>1台使用可</p>
-          </div>
-        </div>
+        {showdata(data)}
       </div>
       <p className="border rounded mx-10"></p>
       <p className="text-center mb-5">あなたの利用状況</p>
       {msg()}
+      <Menu />
     </div>
   );
 }
