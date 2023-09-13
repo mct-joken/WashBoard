05809d63ab56fc0e6f99f6b74e15695a4fd00b5a
kiharu3112 <kijiharu3112@gmail.com>
2023-09-13 22:35:41 +0900

    13:Googleアカウントでログインできるようにした /washページを使うためにはログインが必要になっています
    もしレビューしてくれるときはfirebaseの環境変数をDiscordのDMで渡すので言ってください。(別に自分で作ってもらっても構わないけど)

---
+++ app/firebase/services.ts
@@ -0,0 +1,28 @@
+import { GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { FirebaseAuth } from "./config";
+
+const googleProvider = new GoogleAuthProvider()
+
+export const signInWithGoogle = async () => {
+    try {
+        const result = await signInWithPopup(FirebaseAuth, googleProvider)
+
+
+        const { uid } = result.user
+
+        return uid
+    } catch (e) {
+        alert((e as Error).message)
+    }
+}
+type StateDispatch = any
+
+export const onAuthStateHasChanged = (setSession: StateDispatch) => {
+    onAuthStateChanged(FirebaseAuth, user => {
+        if (!user) return setSession({ status: 'no-authenticated', userId: null})
+        setSession({ status: 'authenticated', userId: user!.uid})
+    })
+}
+
+export const logoutFirebase = async () => await FirebaseAuth.signOut()
+
