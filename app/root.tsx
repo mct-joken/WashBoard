dc0f33dcf73f2f345df0e71b445fb15885c22ee5
tufusa <tufusa74@gmail.com>
2023-09-14 13:29:09 +0900

    fix: サイトの言語をjaに修正

--- app/root.tsx
+++ app/root.tsx
@@ -44,7 +44,7 @@ export default function App() {
   initializeApp(firebaseOptions);
 
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
