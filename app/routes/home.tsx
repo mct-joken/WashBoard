b53396e99440ac26730894ebba4d4fb0c1d3a344
Suzune2741 <kannra1174@gmail.com>
2023-09-16 16:00:19 +0900

    アイコン変更とセレクトボックスのdefault値のコードを変更

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,27 +1,27 @@
 import React from "react";
-import Logo from "../../public/images/logo.png";
-import { json, LinksFunction } from "@remix-run/cloudflare";
+import Logo from "../../public/icons/Group_41.svg";
+import { json } from "@remix-run/cloudflare";
 import { Link, useLoaderData } from "@remix-run/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Menu from "~/components/menu";
 import { LoaderArgs } from "@remix-run/cloudflare";
 import { getLaundries } from "~/models/laundry.server";
-import { laundries } from "~/db/schema";
 import { getRooms } from "~/models/room.server";
+import { getUseById } from "~/models/use.server";
 
 //この辺でデータとか取ってきて自分の使用状況たしかめる
 const ngmsg = "なし";
 let okmsg = "使用中";
-
-
+const id = "IMzzwO0CP60q7glLLMVGV";
 export const loader = async ({ context }: LoaderArgs) => {
   const rooms = await getRooms(context);
   const laundries = await getLaundries(context);
-  return json({ rooms,laundries });
+  const now_use = await getUseById(context, id);
+  return json({ rooms, laundries, now_use });
 };
-
 const is_use = () => {
-  if (true) {
+  const { now_use } = useLoaderData<typeof loader>();
+  if (now_use != null) {
     return (
       <div>
         <p className="text-center mb-5">{okmsg}</p>
@@ -42,13 +42,17 @@ const is_use = () => {
     return <p className="text-center mb-5">{ngmsg}</p>;
   }
 };
-
-
-export function show_select_data(selectdata: string,is_empty: boolean) {
-  const { rooms } = useLoaderData<typeof loader>();
+const is_empty = () => {
   const { laundries } = useLoaderData<typeof loader>();
+  return true;
+};
+
+export function show_select_data(selectdata: string) {
+  const { rooms } = useLoaderData<typeof loader>();
   return rooms.map((element) => {
-    if (selectdata == "empty" && is_empty) {
+    let empty = is_empty();
+    console.log(empty,selectdata);
+    if (selectdata == "empty" && empty) {
       return (
         <div className="flex flex-row justify-center my-2.5 ">
           <div className=" rounded-full bg-blue-400 active:bg-blue-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
@@ -64,11 +68,11 @@ export function show_select_data(selectdata: string,is_empty: boolean) {
         </div>
       );
     } else if (selectdata == "all") {
-      if ({/*is_emptyがfalseなら*/}) {
+      if (empty) {
         return (
           <div className="flex flex-row justify-center my-2.5 ">
-            <div className=" rounded-full bg-red-400 active:bg-red-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-              満
+            <div className=" rounded-full bg-blue-400 active:bg-blue-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
+              空
             </div>
 
             <div className=" text-20 mr-2 text-lg">
@@ -82,8 +86,8 @@ export function show_select_data(selectdata: string,is_empty: boolean) {
       } else {
         return (
           <div className="flex flex-row justify-center my-2.5 ">
-            <div className=" rounded-full bg-blue-400 active:bg-blue-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
-              空
+            <div className=" rounded-full bg-red-400 active:bg-red-400  py-1 px-5 text-white mr-3 p-0 w-30 ">
+              満
             </div>
             <div className=" text-20 mr-2 text-lg">
               <p>{element.place}</p>
@@ -119,9 +123,7 @@ export default function home() {
           <form>
             <div>
               <select name="sort" id="sort" onChange={dataChange}>
-                <option defaultValue="all" >
-                  すべて
-                </option>
+                <option defaultValue="all" value="all">すべて</option>
                 <option value="empty">空きあり</option>
                 <option value="favorite">お気に入り</option>
               </select>
@@ -136,7 +138,7 @@ export default function home() {
       <p className="border rounded mx-10"></p>
       <p className="text-center mb-5">あなたの利用状況</p>
       {is_use()}
-      
+
       <Menu />
     </main>
   );
