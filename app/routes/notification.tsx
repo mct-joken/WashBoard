5cbaa5d6e2cf4cd15dfb5be1c4296fdef3376d85
tufusa <tufusa74@gmail.com>
2023-09-16 14:14:40 +0900

    feat: `/subscribe`を`/notification`にリネーム、sendボタンを追加

--- app/routes/notification.tsx
+++ app/routes/notification.tsx
@@ -1,9 +1,11 @@
-import { ActionArgs, LoaderArgs, json, redirect } from "@remix-run/cloudflare";
+import { LoaderArgs, json, redirect } from "@remix-run/cloudflare";
 import { useFetcher, useLoaderData } from "@remix-run/react";
 import { useEffect, useState } from "react";
 import Menu from "~/components/menu";
 import { requestToken } from "~/firebase/messageServices.client";
-import { getAccountByEmail, updateAccount } from "~/models/account.server";
+import { getAccountByEmail } from "~/models/account.server";
+import { action as subscribeNotificationAction } from "~/routes/api.v1.notification.subscribe";
+import { action as sendNotificationAction } from "~/routes/api.v1.notification.send";
 
 export const loader = async ({ context }: LoaderArgs) => {
   const env = context.env as Env;
@@ -15,21 +17,11 @@ export const loader = async ({ context }: LoaderArgs) => {
   return json({ context, env, account });
 };
 
-export const action = async ({ request, context }: ActionArgs) => {
-  const formData = await request.formData();
-  const accountId = formData.get("accountId");
-  const messageToken = formData.get("messageToken");
-  if (typeof accountId !== "string" || typeof messageToken !== "string") {
-    return;
-  }
-
-  return await updateAccount(context, { id: accountId, messageToken });
-};
-
-const Subscribe = () => {
+const NotificationTest = () => {
   const { env, account } = useLoaderData<typeof loader>();
   const [currentAccount, setCurrentAccount] = useState(account);
-  const setMessageToken = useFetcher<typeof action>();
+  const setMessageToken = useFetcher<typeof subscribeNotificationAction>();
+  const sendNotification = useFetcher<typeof sendNotificationAction>();
 
   const onSubscribe = async () => {
     const token = await requestToken(env.FIREBASE_VAPID_SERVER_KEY);
@@ -42,7 +34,22 @@ const Subscribe = () => {
         accountId: currentAccount.id,
         messageToken: token,
       },
-      { method: "POST" }
+      { action: "/api/v1/notification/subscribe", method: "POST" }
+    );
+  };
+
+  const onSend = async () => {
+    if (currentAccount.messageToken == null) {
+      return;
+    }
+
+    sendNotification.submit(
+      {
+        to: currentAccount.messageToken,
+        notificationTitle: "Test",
+        notificationBody: "Notification",
+      },
+      { action: "/api/v1/notification/send", method: "POST" }
     );
   };
 
@@ -51,13 +58,17 @@ const Subscribe = () => {
     setCurrentAccount(setMessageToken.data);
   }, [setMessageToken.data]);
 
+  useEffect(() => {
+    console.log(sendNotification.data);
+  }, [sendNotification.data]);
+
   return (
     <>
       <div className="p-3">
         <button
           onClick={onSubscribe}
           className="
-            p-2 border rounded-lg
+            m-2 p-2 border rounded-lg
             shadow
             hover:bg-gray-300
             active:bg-slate-500 active:text-white active:shadow-none
@@ -65,10 +76,21 @@ const Subscribe = () => {
         >
           Subscribe notification
         </button>
+        <button
+          onClick={onSend}
+          className="
+            m-2 p-2 border rounded-lg
+            shadow
+            hover:bg-gray-300
+            active:bg-slate-500 active:text-white active:shadow-none
+          "
+        >
+          Send notification
+        </button>
       </div>
       <Menu />
     </>
   );
 };
 
-export default Subscribe;
+export default NotificationTest;
